1b5352a1ace501688cac8a1a4d5dc6ee5793bde2
Muziljohn <muzilhassan9@gmail.com>
2023-06-09 22:55:30 +0500

    Started working on voiceAssitant news app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { useEffect } from "react";
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: "e5a8cc5afe1582651c2aaf1ccdd8ee752e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command }) => {
+        if (command === "test command") {
+          alert("this command was executed");
+        }
+      },
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bayanguu wa</h1>
     </div>
   );
 }
